dd29707f2a69fa75c8305c01df9fbe29507fba1c
Kentaro Wuertele <ken@wuertele.com>
2025-04-08 16:05:46 -0700

    Currently finished with the token types and functions.

--- src/eweda.cpp
+++ src/eweda.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
+#include <vector>
 
 #include "token.hpp"
 
+using namespace eweda;
+
 int main (int argc, char *argv[]) {
-  std::cout << "Hello\n";
+  std::vector tokens {
+    token::Token{token::Token_Type::COMMA, "(", 3},
+    token::Token{token::Token_Type::STRING_LITERAL, "hello world", std::optional<token::Literal>("hello world"), 5},
+  };
+
+  for (const auto &t : tokens) {
+    std::cout << token::to_string(t) << '\n';
+  }
   
   return 0;
 }
--- src/token.hpp
+++ src/token.hpp
@@ -1,5 +1,50 @@
 #pragma once
 
+#include <string>
+#include <variant>
+#include <optional>
+
 namespace eweda::token {
+
+  enum class Token_Type {
+    // Single character
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACKET, RIGHT_BRACKENT, LEFT_BRACE, RIGHT_BRACE,
+    COMMA, DOT, SLASH, STAR, SEMICOLON,
+
+    // One or two character
+    BANG, BANG_EQUAL,
+    EQUAL, EQUAL_EQUAL,
+    GREATER, GREATER_EQUAL,
+    LESS, LESS_EQUAL,
+    AND, AND_AND, // These correspond to the symbols, not the words.
+    OR, OR_OR,    // `and` and `or` are both used to refer to the keyboard symbols anyway.
+
+    // Literals
+    STRING_LITERAL, INTEGER_LITERAL, FLOAT_LITERAL, BOOLEAN_LITERAL, NULL_LITERAL,
+
+    // Keywords
+    BREAK, ELSE, FOR, IF, LET, PRINT, RETURN, WHILE,
+
+    IDENTIFIER,
+
+    END_OF_FILE
+  };
+
+  using Literal = std::variant<std::string, int, double, bool>;
+
+  struct Token {
+    const Token_Type type;
+    const std::string lexeme;
+    const std::optional<Literal> literal {};
+    const int line; // Line number of the program at which the token appears.
+
+    Token (const Token_Type type, const std::string lexeme, const std::optional<Literal> literal, const int line);
+
+    Token (const Token_Type type, const std::string lexeme, const int line);
+  };
+
+  std::string to_string (const Literal &literal);
+
+  std::string to_string (const Token &token);
 
 } // namespace eweda::token
