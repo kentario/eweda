8532fb68dfb227f3d14cf5cccfd91bea25c622ff
Kentaro Wuertele <ken@wuertele.com>
2025-04-08 20:06:11 -0700

    Slight syntax changes.

--- src/token.cpp
+++ src/token.cpp
@@ -1,49 +1,50 @@
-#include <unordered_map>
-#include <type_traits>
-
 #include "token.hpp"
+
+#include <unordered_map>
+#include <map>
+#include <type_traits>
 
 namespace eweda::token {
 
   const std::unordered_map<Token_Type, std::string> token_to_string {
-    {Token_Type::LEFT_PAREN, "LEFT_PAREN"},
-    {Token_Type::RIGHT_PAREN, "RIGHT_PAREN"},
-    {Token_Type::LEFT_BRACKET, "LEFT_BRACKET"},
-    {Token_Type::RIGHT_BRACKENT, "RIGHT_BRACKENT"},
-    {Token_Type::LEFT_BRACE, "LEFT_BRACE"},
-    {Token_Type::RIGHT_BRACE, "RIGHT_BRACE"},
-    {Token_Type::COMMA, "COMMA"},
-    {Token_Type::DOT, "DOT"},
-    {Token_Type::SLASH, "SLASH"},
-    {Token_Type::STAR, "STAR"},
-    {Token_Type::SEMICOLON, "SEMICOLON"},
-    {Token_Type::BANG, "BANG"},
-    {Token_Type::BANG_EQUAL, "BANG_EQUA"},
-    {Token_Type::EQUAL, "EQUAL"},
-    {Token_Type::EQUAL_EQUAL, "EQUAL_EQUA"},
-    {Token_Type::GREATER, "GREATER"},
-    {Token_Type::GREATER_EQUAL, "GREATER_EQUAL"},
-    {Token_Type::LESS, "LESS"},
-    {Token_Type::LESS_EQUAL, "LESS_EQUAL"},
-    {Token_Type::AND, "AND"},
-    {Token_Type::AND_AND, "AND_AND"},
-    {Token_Type::OR, "OR"},
-    {Token_Type::OR_OR, "OR_OR"},
-    {Token_Type::STRING_LITERAL, "STRING_LITERAL"},
+    {Token_Type::LEFT_PAREN,      "LEFT_PAREN"},
+    {Token_Type::RIGHT_PAREN,     "RIGHT_PAREN"},
+    {Token_Type::LEFT_BRACKET,    "LEFT_BRACKET"},
+    {Token_Type::RIGHT_BRACKET,   "RIGHT_BRACKENT"},
+    {Token_Type::LEFT_BRACE,      "LEFT_BRACE"},
+    {Token_Type::RIGHT_BRACE,     "RIGHT_BRACE"},
+    {Token_Type::COMMA,           "COMMA"},
+    {Token_Type::DOT,             "DOT"},
+    {Token_Type::SLASH,           "SLASH"},
+    {Token_Type::STAR,            "STAR"},
+    {Token_Type::SEMICOLON,       "SEMICOLON"},
+    {Token_Type::BANG,            "Bang"},
+    {Token_Type::BANG_EQUAL,      "BANG_EQUA"},
+    {Token_Type::EQUAL,           "EQUAL"},
+    {Token_Type::EQUAL_EQUAL,     "EQUAL_EQUA"},
+    {Token_Type::GREATER,         "GREATER"},
+    {Token_Type::GREATER_EQUAL,   "GREATER_EQUAL"},
+    {Token_Type::LESS,            "LESS"},
+    {Token_Type::LESS_EQUAL,      "LESS_EQUAL"},
+    {Token_Type::AND,             "AND"},
+    {Token_Type::AND_AND,         "AND_AND"},
+    {Token_Type::OR,              "OR"},
+    {Token_Type::OR_OR,           "OR_OR"},
+    {Token_Type::STRING_LITERAL,  "STRING_LITERAL"},
     {Token_Type::INTEGER_LITERAL, "INTEGER_LITERAL"},
-    {Token_Type::FLOAT_LITERAL, "FLOAT_LITERAL"},
+    {Token_Type::FLOAT_LITERAL,   "FLOAT_LITERAL"},
     {Token_Type::BOOLEAN_LITERAL, "BOOLEAN_LITERAL"},
-    {Token_Type::NULL_LITERAL, "NULL_LITERAL"},
-    {Token_Type::BREAK, "BREAK"},
-    {Token_Type::ELSE, "ELSE"},
-    {Token_Type::FOR, "FOR"},
-    {Token_Type::IF, "IF"},
-    {Token_Type::LET, "LET"},
-    {Token_Type::PRINT, "PRINT"},
-    {Token_Type::RETURN, "RETURN"},
-    {Token_Type::WHILE, "WHILE"},
-    {Token_Type::IDENTIFIER, "IDENTIFIER"},
-    {Token_Type::END_OF_FILE, "END_OF_FILE"}
+    {Token_Type::NULL_LITERAL,    "NULL_LITERAL"},
+    {Token_Type::BREAK,           "BREAK"},
+    {Token_Type::ELSE,            "ELSE"},
+    {Token_Type::FOR,             "FOR"},
+    {Token_Type::IF,              "IF"},
+    {Token_Type::LET,             "LET"},
+    {Token_Type::PRINT,           "PRINT"},
+    {Token_Type::RETURN,          "RETURN"},
+    {Token_Type::WHILE,           "WHILE"},
+    {Token_Type::IDENTIFIER,      "IDENTIFIER"},
+    {Token_Type::END_OF_FILE,     "END_OF_FILE"}
   };
 
   Token::Token (const Token_Type type, const std::string lexeme, const std::optional<Literal> literal, const size_t line) :
