7dd02a630e79bc1afd008c601b4329dcda0d151a
Kentaro Wuertele <ken@wuertele.com>
2025-04-09 18:27:56 -0700

    File reading and tokenization functionality.

--- src/eweda.cpp
+++ src/eweda.cpp
@@ -1,18 +1,39 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
 #include <vector>
 
-#include "token.hpp"
+#include "error.hpp"
+#include "lexer.hpp"
 
 using namespace eweda;
 
 int main (int argc, char *argv[]) {
-  std::vector tokens {
-    token::Token{token::Token_Type::COMMA, "(", 3},
-    token::Token{token::Token_Type::STRING_LITERAL, "hello world", std::optional<token::Literal>("hello world"), 5},
-  };
+  if (argc != 2) {
+    std::cerr << "Usage: " << argv[0] << " [script]\n";
+    return EXIT_FAILURE;
+  }
 
+  const std::string filename {argv[1]};
+  std::ifstream file {filename};
+
+  if (!file.is_open()) {
+    std::cerr << "error: could not open file '" << filename << "'\n";
+  }
+
+  std::stringstream buff;
+  buff << file.rdbuf();
+  const std::string src {buff.str()};
+
+  lexer::Lexer lexer {src};
+  std::vector tokens {lexer.tokenize()};
+  
   for (const auto &t : tokens) {
     std::cout << token::to_string(t) << '\n';
+  }
+
+  if (error::had_error) {
+    return EXIT_FAILURE;
   }
   
   return 0;
