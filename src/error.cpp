046d67c062a25a01bbe26b5262bbcefc2b9dae7b
Kentaro Wuertele <ken@wuertele.com>
2025-04-11 12:56:00 -0700

    More testing, adding in executables, and better documentation.

--- src/error.cpp
+++ src/error.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <string>
 
+#include "token.hpp"
+
 namespace eweda::error {
 
   bool had_error {false};
@@ -13,8 +15,16 @@ namespace eweda::error {
   }
 
   void lexer_error (const size_t line, const std::string &message) {
-    std::cerr << "lexer error at line "  << line << ": " << message << '\n';
+    std::cerr << "line " << line << ": lexer error: " << message << '\n';
     had_error = true;
+  }
+
+  void parse_error (const token::Token &token, const std::string &message) {
+    if (token.type == token::Token_Type::END_OF_FILE) {
+      std::cerr << "line " << token.line << ": parse error at end: " << message << '\n';
+    } else {
+      std::cerr << "line " << token.line << ": parse error: " << message << '\n';
+    }
   }
 
 } // namespace eweda::error
--- src/error.hpp
+++ src/error.hpp
@@ -3,12 +3,20 @@
 #include <iostream>
 #include <string>
 
+#include "token.hpp"
+
 namespace eweda::error {
 
   extern bool had_error;
-  
+
+  // error: message
   void error (const std::string &message);
 
+  // line line: lexer error: message
   void lexer_error (const size_t line, const std::string &message);
+
+  // line line: parse error at end: message
+  // line line: parse error: message
+  void parse_error (const token::Token &token, const std::string &message);
     
 } // namespace eweda::error
